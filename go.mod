9ab8ac6f2df45348c8af16a355bb3c08e1c7d66a
jigsaw <j1g5aw@foxmail.com>
2022-04-06 22:37:48 +0800

    feat: output as markdown,html, send mail

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module simpread-sync
 
 go 1.18
+
+require (
+	github.com/valyala/fastjson v1.6.3
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+)
+
+require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
--- main.go
+++ main.go
@@ -2,16 +2,74 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/valyala/fastjson"
+	"gopkg.in/gomail.v2"
 )
 
-//未验证 json 返回 403
-//已验证 json 返回 201
+type Config struct {
+	port         int
+	syncPath     string
+	outputPath   string
+	smtpHost     string
+	smtpPort     int
+	username     string
+	password     string
+	receiverMail string
+	title        string
+	kindleMail   string
+}
+
+func initConfig(path string) *Config {
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := &Config{
+		port:         fastjson.GetInt(data, "port"),
+		syncPath:     fastjson.GetString(data, "syncPath"),
+		outputPath:   fastjson.GetString(data, "outputPath"),
+		title:        fastjson.GetString(data, "title"),
+		smtpHost:     fastjson.GetString(data, "smtpHost"),
+		smtpPort:     fastjson.GetInt(data, "smtpPort"),
+		username:     fastjson.GetString(data, "username"),
+		password:     fastjson.GetString(data, "password"),
+		receiverMail: fastjson.GetString(data, "receiverMail"),
+		kindleMail:   fastjson.GetString(data, "kindleMail"),
+	}
+
+	if config.port == 0 {
+		config.port = 7026
+	}
+	if config.smtpPort == 0 {
+		config.smtpPort = 465
+	}
+	if config.title == "" {
+		config.title = "[简悦] - %s"
+	}
+
+	log.Println("init config")
+
+	return config
+}
+
+// 未验证 json 返回 201
+// 已验证 json 返回 403
+// 这里无论如何都返回成功，有其他用处以后再说
 func verifyHandle(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
 	result, err := json.Marshal(struct {
 		Code   int    `json:"code"`
 		Status string `json:"status"`
@@ -30,14 +88,17 @@ func verifyHandle(w http.ResponseWriter, r *http.Request) {
 	log.Println("verify success")
 }
 
-// 如果浏览器插件的设置项更改了，它会发一个 key 为 config 的 urlencoded 请求，json 返回 200
-// 如果本地文件更改了，返回一个 key 为 result 的 json
-func configHandle(w http.ResponseWriter, r *http.Request, path string) {
+// 如果浏览器插件的设置项更改了，它会发一个 key 为 config 的请求，json 返回 200
+// 剩余情况下，返回一个 key 为 result 的 json
+func configHandle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
-	r.ParseForm()
-	if config := r.Form.Get("config"); config != "" {
-		err := ioutil.WriteFile(path, []byte(config), 644)
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if data := r.Form.Get("config"); data != "" {
+		err := ioutil.WriteFile(filepath.Join(config.syncPath, "simpread_config.json"), []byte(data), 644)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -55,7 +116,7 @@ func configHandle(w http.ResponseWriter, r *http.Request, path string) {
 		}
 		log.Println("sync config from browser")
 	} else {
-		config, err := ioutil.ReadFile(path)
+		config, err := ioutil.ReadFile(filepath.Join(config.syncPath, "simpread_config.json"))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -78,20 +139,105 @@ func configHandle(w http.ResponseWriter, r *http.Request, path string) {
 	}
 }
 
+func plainHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	title := r.Form.Get("title")
+	content := r.Form.Get("content")
+
+	err = ioutil.WriteFile(filepath.Join(config.outputPath, title), []byte(content), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := json.Marshal(struct {
+		Status int `json:"status"`
+	}{Status: 200})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = w.Write(result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("save file: %s\n", title)
+}
+
+// TODO 发送后删除附件
+func mailHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	title := r.Form.Get("title")
+	content := r.Form.Get("content")
+	attach := r.Form.Get("attach")
+
+	d := gomail.NewDialer(config.smtpHost, config.smtpPort, config.username, config.password)
+	s, err := d.Dial()
+	if err != nil {
+		panic(err)
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", config.username)
+	m.SetHeader("To", config.receiverMail)
+	m.SetHeader("Subject", fmt.Sprintf(config.title, title))
+	m.SetBody("text/html", content)
+
+	var attachPath string
+	if attach != "" {
+		attachPath = fmt.Sprintf("tmp-%s.%s", title, attach)
+		m.Attach(attachPath)
+	}
+
+	err = gomail.Send(s, m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if attach != "" {
+		os.Remove(attachPath)
+	}
+
+	result, err := json.Marshal(struct {
+		Status int `json:"status"`
+	}{Status: 200})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = w.Write(result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("send mail: %s\n", title)
+}
+
+var config *Config
+
 func main() {
 	var path string
 	if len(os.Args) < 2 {
-		path = "simpread_config.json"
+		path = "config.json"
 	} else {
 		path = os.Args[1]
 	}
-	log.Println("config file path:", path)
+	config = initConfig(path)
 
 	http.HandleFunc("/verify", verifyHandle)
-	http.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) { configHandle(w, r, path) })
+	http.HandleFunc("/config", configHandle)
+	http.HandleFunc("/plain", plainHandle)
+	http.HandleFunc("/mail", mailHandle)
 
-	err := http.ListenAndServe(":7026", nil)
+	err := http.ListenAndServe(fmt.Sprint(":", config.port), nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal(err)
 	}
 }
